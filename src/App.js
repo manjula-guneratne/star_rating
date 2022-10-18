a5edf6e920f4ea809e5f5628e1c22431b0b68956
Manjula <mguneratne3@gmail.com>
2022-10-18 14:04:31 -0400

    set the ability to select the number of stars to be visible

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from "react";
 import StarRating from "./StarRating";
 
 function App() {
-  return <StarRating/>;
+  return <StarRating totalStars={10}/>;
 }
 
 export default App;
--- src/StarRating.js
+++ src/StarRating.js
@@ -1,14 +1,16 @@
 import React from "react";
 import {FaStar} from "react-icons/fa";
 
-function StarRating(){
-    return[
-        <FaStar color="red" />,
-        <FaStar color="red" />,
-        <FaStar color="red" />,
-        <FaStar color="grey" />,
-        <FaStar color="grey" />
-    ];
+function StarRating({ totalStars = 5 }){  //5 stars is pre-set here
+
+    const Star = ({ selected = false}) => (
+        <FaStar color={selected ? "red": "grey"} />
+    )
+    const createArray = length => [...Array(length)];
+
+    console.log('CreateArray lengh = '+createArray);
+
+    return createArray(totalStars).map((n,i) => <Star key={i} />);
 }
  
 export default StarRating;
