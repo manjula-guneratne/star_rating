55e2ec21b02bcc793634f331127ca9efaa4e87e1
Manjula <mguneratne3@gmail.com>
2022-10-19 14:10:54 -0400

    css color added

--- src/App.js
+++ src/App.js
@@ -2,7 +2,13 @@ import React from "react";
 import StarRating from "./StarRating";
 
 function App() {
-  return <StarRating totalStars={10}/>;
+  return (
+    <StarRating
+      totalStars={10}
+      style={{ backgroundColor: "lightblue" }}
+      onDoubleClick={(e) => alert("double clicked!")}
+    />
+  );
 }
 
 export default App;
--- src/StarRating.js
+++ src/StarRating.js
@@ -1,23 +1,30 @@
 import React, { useState } from "react";
 import { FaStar } from "react-icons/fa";
 
-function StarRating({ totalStars = 5 }) { //5 stars is pre-set here
-  
+function StarRating({ totalStars = 5, style = {} }) {
+  //5 stars is pre-set here
+
   const [selectedStars, setSelectedStars] = useState(0);
 
   const Star = ({ selected = false, onSelect = (f) => f }) => (
-    <FaStar color={selected ? "red" : "grey"} onClick={onSelect}/>
+    <FaStar color={selected ? "red" : "grey"} onClick={onSelect} />
   );
   const createArray = (length) => [...Array(length)];
 
   return (
     <>
-      {createArray(totalStars).map((n, i) => (
-        <Star key={i} selected={selectedStars > i} onSelect={() => setSelectedStars(i+1)} />
-      ))}
-      <p>
-        {selectedStars} of {totalStars} stars
-      </p>
+      <div style={{ padding: "5px", ...style }}>
+        {createArray(totalStars).map((n, i) => (
+          <Star
+            key={i}
+            selected={selectedStars > i}
+            onSelect={() => setSelectedStars(i + 1)}
+          />
+        ))}
+        <p>
+          {selectedStars} of {totalStars} stars
+        </p>
+      </div>
     </>
   );
 }
