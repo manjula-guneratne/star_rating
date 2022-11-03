8839be906659f3c27786b40a445254530e084284
Manjula <mguneratne3@gmail.com>
2022-11-03 16:48:55 -0400

    error corrected

--- src/App.js
+++ src/App.js
@@ -1,26 +1,40 @@
 import React, { useState } from "react";
 import colourData from "./colour-data.json";
 import ColourList from "./ColourList.js";
+import AddColourForm from "./AddColourForm";
+import { v4 } from "uuid";
 
 function App() {
   const [colours, setColours] = useState(colourData);
 
-  const removeColour = id => {
-    const newColours = colours.filter(colour => colour.id !== id);
-    setColours(newColours);
-  };
+  // const removeColour = id => {
+  //   const newColours = colours.filter(colour => colour.id !== id);
+  //   setColours(newColours);
+  // };
 
-  const rateColour = (id, rating) => {
-    const newColours = colours.map(colour => colour.id === id ? {...colour, rating} : colour);
-    setColours(newColours);
-  };
+  // const rateColour = (id, rating) => {
+  //   const newColours = colours.map(colour => colour.id === id ? {...colour, rating} : colour);
+  //   setColours(newColours);
+  // };
 
   return (
-    <ColourList
-      colours={colours}
-      onRemoveColour={removeColour}
-      onRateColour={rateColour}      
-    />
+    <>
+      <AddColourForm
+        onNewColour={(title, colour) => {
+          const newColours = [
+            ...colours,
+            {
+              id: v4(),
+              rating: 0,
+              title,
+              colour,
+            },
+          ];
+          setColours(newColours);
+        }}
+      />
+      <ColourList />
+    </>
   );
 }
 
